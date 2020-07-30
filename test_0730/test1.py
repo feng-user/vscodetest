313b6ed7a47bd7e521d1e23f46bc22fb546f0ba9
feng-user <2453425628@qq.com>
2020-07-30 19:56:06 +0800

    'redis with python test'

--- test_0730/test1.py
+++ test_0730/test1.py
@@ -1,3 +1,17 @@
-
 from redis import *
 
+sr = StrictRedis(host='192.168.126.135', port=6379, password='')
+try:
+    result = sr.set('py1', 'gj')
+    print(result)
+except Exception as e:
+    print(e)
+
+# result = sr.delete('py1')
+# print(result)
+
+result = sr.set('py1', 'he')
+
+result = sr.get('py1')
+
+result= sr.keys
